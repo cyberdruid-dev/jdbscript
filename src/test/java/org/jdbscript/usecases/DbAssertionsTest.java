f16e08d8c9b3964d7014da05dfc9beef25b68757
CyberDruidDev <cyberdruid.dev@gmail.com>
2026-08-31 17:46:46 +0000

    more DbAssertionsTest tests, add missing 003-assertions.yaml

--- src/test/java/org/jdbscript/usecases/DbAssertionsTest.java
+++ src/test/java/org/jdbscript/usecases/DbAssertionsTest.java
@@ -33,25 +33,95 @@ public class DbAssertionsTest extends JdbAbstractTest {
     }
 
     private final IJDBEngine<IAssertionTestSchema> engine = createEngine(IAssertionTestSchema.class);
-
-    public static abstract class AssertionsDataset implements IAssertionTestSchema {{
+    public static Date date1;
+    public static Date date2;
+    static {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2010, 5, 6, 17, 18, 29);
-        Date date1 =  calendar.getTime();
+        date1 =  calendar.getTime();
         calendar.set(2011, 6, 16, 17, 18, 29);
-        Date date2 =  calendar.getTime();
+        date2 =  calendar.getTime();
+    }
+
+    public static abstract class AssertionsDataset implements IAssertionTestSchema {{
         table_for_assertions().str_column_1("str11").str_column_2("str21")
                 .boolean_column_1(true).int_column_1(101).date_column_1(date1);
         table_for_assertions().str_column_1("str12").str_column_2("str22")
                 .boolean_column_1(false).int_column_1(102).date_column_1(date2);
     }};
 
-    public void assertDBHas__passes_if__row_with_column__value_exists() {
+
+    public void assertDBHas__passes_if__row_with_multiple_column_values_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        engine.assertDBHas(db -> {
+            db.table_for_assertions().str_column_1("str11").int_column_1(101);
+        });
+    }
+
+    public void assertDBHas__passes_if__row_with_boolean_column_values_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        engine.assertDBHas(db -> {
+            db.table_for_assertions().str_column_1("str11").boolean_column_1(true);
+        });
+    }
+
+    public void assertDBHas__passes_if__row_with_date_column_values_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        engine.assertDBHas(db -> {
+            db.table_for_assertions().str_column_1("str11").date_column_1(date1);
+        });
+    }
+
+    public void assertDBHas__passes_if__row_with_date_column_value_NOT_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        expectFailure(()-> {
+            engine.assertDBHas(db -> {
+                db.table_for_assertions().str_column_1("str11").date_column_1(date2);
+            });
+        }, new AssertionFailedError("Expected row to exist."));
+    }
+
+    public void assertDBHas__FAIL_if__row_with_multiple_column_values_NOT_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        expectFailure(()-> {
+            engine.assertDBHas(db->{
+                db.table_for_assertions().str_column_1("str11").int_column_1(102);
+            });
+        }, new AssertionFailedError("Expected row to exist."));
+    }
+
+    public void assertDBHas__passes_if__row_with_column_value_exists() {
         engine.resetDB(AssertionsDataset.class);
 
         engine.assertDBHas(db->{
             db.table_for_assertions().str_column_1("str11");
         });
+    }
+
+    public void assertDBHas__passes_if__multiple_row_with_column__value_exist() {
+        engine.resetDB(AssertionsDataset.class);
+
+        engine.assertDBHas(db->{
+            db.table_for_assertions().str_column_1("str11");
+            db.table_for_assertions().str_column_1("str12");
+        });
+    }
+
+    @Test
+    public void assertDBHas__FAILS_if__one_of_multiple_row_with_column_value__NOT_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        expectFailure(()-> {
+            engine.assertDBHas(db -> {
+                db.table_for_assertions().str_column_1("str11");
+                db.table_for_assertions().str_column_1("strXX");
+            });
+        }, new AssertionFailedError("Expected row to exist."));
     }
 
     @Test
@@ -75,12 +145,34 @@ public class DbAssertionsTest extends JdbAbstractTest {
     }
 
     @Test
+    public void assertDBHasNot__passes_if_non_of_multiple_row_with_column_value_exists() {
+        engine.resetDB(AssertionsDataset.class);
+
+        engine.assertDBHasNot(db->{
+            db.table_for_assertions().str_column_1("strXX");
+            db.table_for_assertions().str_column_1("strYY");
+        });
+    }
+
+    @Test
     public void assertDBHasNot__FAIL_if_row_with_column_value_EXISTS() {
         engine.resetDB(AssertionsDataset.class);
 
         expectFailure(()->{
             engine.assertDBHasNot(db->{
                 db.table_for_assertions().str_column_1("str11");
+            });
+        }, new AssertionFailedError("Expected row to NOT exist."));
+    }
+
+    @Test
+    public void assertDBHasNot__FAIL_if_any_row_with_column_value_EXISTS() {
+        engine.resetDB(AssertionsDataset.class);
+
+        expectFailure(()->{
+            engine.assertDBHasNot(db->{
+                db.table_for_assertions().str_column_1("str11");
+                db.table_for_assertions().str_column_1("strXX");
             });
         }, new AssertionFailedError("Expected row to NOT exist."));
     }
